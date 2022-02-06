b1c3a45666c689ebf2bd7bd596f49b45aea737e6
dindin12138 <din12138@outlook.com>
2022-02-06 20:12:03 +0800

    Add prompt message

--- zsh/proxy.zsh
+++ zsh/proxy.zsh
@@ -1,9 +1,16 @@
 # Set Proxy
 function setproxy() {
     export {http,https,frp}_proxy="http://127.0.0.1:8889"
+    echo -e "Terminal proxy is enabled"
 }
 
 # Unset Proxy
 function unsetproxy() {
     unset {http,https,ftp}_proxy
+    echo -e "Terminal proxy is disabled"
+}
+
+# Cat Proxy
+function catproxy() {
+    echo $http_proxy
 }
