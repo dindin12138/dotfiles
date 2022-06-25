93302d311fc2c1cfe5b96f7d37fbc3780e670ea8
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-06-25 22:28:13 +0800

    refactor: arrange the script

---
+++ scripts/wallpaper-wrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+killall -9 xwinwrap
+
+wp=$(cat $HOME/.config/scripts/wallpaper-save.sh)
+
+xwinwrap -g 1920x1080+0+0 -un -fdt -ni -b -nf -ov -- $HOME/.config/scripts/wallpaper-live.sh WID $wp &
+#xwinwrap -g 1920x1080+1920+0 -un -fdt -ni -b -nf -ov -- $HOME/.dotfiles/tag-scripts/scripts/wallpaper-live.sh WID $wp &
+#xwinwrap -g 1920x1080+3840+0 -un -fdt -ni -b -nf -ov -- $HOME/scripts/wallpaper-live.sh WID $wp &
---
+++ scripts/wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+feh --bg-fill --no-fehbg ~/Pictures/Background/ ~/Pictures/Background_hdmi 
+	
