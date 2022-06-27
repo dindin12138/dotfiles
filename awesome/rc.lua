00e3754677f15a4a3ebf6d9f0a17e03c90e73d0f
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-06-28 00:46:42 +0800

    feat: delete menu config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -97,28 +97,8 @@ awful.layout.layouts = {
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
--- myawesomemenu = {
---     { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
---     { "manual", terminal .. " -e man awesome" },
---     { "edit config", editor_cmd .. " " .. awesome.conffile },
---     { "restart", awesome.restart },
---     { "quit", function() awesome.quit() end },
--- }
-
-local mymainmenu = awful.menu({ items = {
-    -- { "awesome", myawesomemenu, beautiful.awesome_icon },
-}
-})
-
-local mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-    menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
+local mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = awful.menu() })
 -- }}}
-
--- Keyboard map indicator and switcher
--- mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
@@ -262,7 +242,7 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({}, 3, function() mymainmenu:toggle() end),
+-- awful.button({}, 3, function() mymainmenu:toggle() end),
     awful.button({}, 4, awful.tag.viewnext),
     awful.button({}, 5, awful.tag.viewprev)
 ))
