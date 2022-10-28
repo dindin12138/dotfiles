299ec3eaaac9dc3c6f642e4d7d31dab5c4a1f62d
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-10-28 21:42:02 +0800

    feat: update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
+## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
 
-DIR="$HOME/.config/polybar"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CARD="$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)"
+INTERFACE="$(ip link | awk '/state UP/ {print $2}' | tr -d :)"
+RFILE="$DIR/.module"
 
-# Terminate already running bar instances
-killall -q polybar
+# Fix backlight and network modules
+fix_modules() {
+	if [[ -z "$CARD" ]]; then
+		sed -i -e 's/backlight/bna/g' "$DIR"/config.ini
+	elif [[ "$CARD" != *"intel_"* ]]; then
+		sed -i -e 's/backlight/brightness/g' "$DIR"/config.ini
+	fi
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+	if [[ "$INTERFACE" == e* ]]; then
+		sed -i -e 's/network/ethernet/g' "$DIR"/config.ini
+	fi
+}
 
 # Launch the bar
-polybar -q main -c "$DIR"/config &
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+	polybar -q main -c "$DIR"/config.ini &
+}
+
+# Execute functions
+if [[ ! -f "$RFILE" ]]; then
+	fix_modules
+	touch "$RFILE"
+fi	
+launch_bar
