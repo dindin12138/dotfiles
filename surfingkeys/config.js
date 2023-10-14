f1590aba6dca08cafa77ccb9411611571b3ab84e
Din <79844759+dindin12138@users.noreply.github.com>
2023-10-14 23:44:05 +0800

    feat: add search engines

--- surfingkeys/config.js
+++ surfingkeys/config.js
@@ -59,4 +59,39 @@ map('J', 'R');
 map('>', '>>');
 map('<', '<<');
 
-addSearchAlias('p', 'github', 'https://github.com/search?q=', 's');
+// ---- Unmap -----
+unmap('S');
+unmap('D');
+unmap('E');
+unmap('R');
+
+// ---- Search Engines -----
+removeSearchAlias('b', 's');
+addSearchAlias('bd', 'baidu', 'https://www.baidu.com/s?wd=', 's');
+removeSearchAlias('d', 's');
+addSearchAlias('dd', 'duckduckgo', 'https://duckduckgo.com/?q=', 's');
+removeSearchAlias('g', 's');
+addSearchAlias('gg', 'google', 'https://www.google.com.hk/search?q=', 's');
+removeSearchAlias('h', 's');
+addSearchAlias('gh', 'github', 'https://github.com/search?q=', 's');
+removeSearchAlias('w', 's');
+addSearchAlias('bi', 'bing', 'https://www.bing.com/search?q=', 's');
+removeSearchAlias('y', 's');
+addSearchAlias('yt', 'youtube', 'https://www.youtube.com/results?search_query=', 's');
+removeSearchAlias('s', 's');
+addSearchAlias('so', 'stackoverflow', 'https://stackoverflow.com/search?q=', 's');
+removeSearchAlias('e', 's');
+addSearchAlias('wk', 'wikipedia', 'https://en.wikipedia.org/wiki/Special:Search/', 's');
+// --------------------
+addSearchAlias('bl', 'bilibili', 'https://search.bilibili.com/all?keyword=', 's');
+addSearchAlias('z', 'zhihu', 'https://www.zhihu.com/search?type=content&q=', 's');
+addSearchAlias('a', 'amazon', 'https://www.amazon.com/s?k=', 's');
+addSearchAlias('ap', 'arch pkg', 'https://www.archlinux.org/packages/?sort=&q=', 's');
+addSearchAlias('aur', 'aur', 'https://aur.archlinux.org/packages/?O=0&SeB=nd&K=', 's');
+addSearchAlias('aw', 'arch wiki', 'https://wiki.archlinux.org/index.php?title=Special:Search&search=', 's');
+addSearchAlias('dh', 'docker hub', 'https://hub.docker.com/search?type=image&q=', 's');
+addSearchAlias('fh', 'flathub', 'https://flathub.org/apps/search/', 's');
+addSearchAlias('pt', 'proton', 'https://www.protondb.com/search?q=', 's');
+addSearchAlias('r', 'reddit', 'https://libreddit.spike.codes/r/', 's');
+addSearchAlias('st', 'steam', 'https://store.steampowered.com/search/?term=', 's');
+addSearchAlias('id', 'invidious', 'https://invidious.snopyta.org/search?q=', 's');
