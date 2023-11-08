64807d620ffd1ada638bb0f6c49bf48d669e7363
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2023-11-08 18:42:12 +0800

    feat: add GTK & QT theme config

--- hypr/scripts/gtkthemes-bkp.sh
+++ hypr/scripts/gtkthemes-bkp.sh
@@ -4,12 +4,12 @@
 
 THEME='Catppuccin-Mocha-Standard-Blue-Dark'
 ICONS='Papirus-Dark'
-FONT='FiraCode Nerd Font 10'
+FONT='FiraCode Nerd Font 11'
 CURSOR='Sweet-cursors'
 
 SCHEMA='gsettings set org.gnome.desktop.interface'
 
-apply_themes () {
+apply_themes() {
 	${SCHEMA} gtk-theme "$THEME"
 	${SCHEMA} icon-theme "$ICONS"
 	${SCHEMA} cursor-theme "$CURSOR"
