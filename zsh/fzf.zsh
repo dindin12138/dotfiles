711b1c762f7476af8a499c4f559b278121431da4
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2023-11-26 15:06:29 +0800

    feat: update fzf & joshuto keymap

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,6 +4,11 @@ export FZF_DEFAULT_OPTS="--prompt=' FZF 󰄾 ' --height 50% --layout reverse 
     --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,dist,vendor} --type f"
 
+export _ZO_FZF_OPTS="--prompt=' FZF 󰄾 ' --height 50% --layout reverse --border rounded \
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 function f() {
     if [[ -n "$1" ]]; then
         local original_dir=$(pwd)
